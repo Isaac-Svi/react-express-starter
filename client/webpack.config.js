f728d3572528b1dc69580948b3471510b1cae3d1
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-02-08 19:58:19 +0200

    clean up webpack dev server output

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 const {
   proxy: { host, target },
-} = require('./package.json');
+} = require('./package.json')
 
 module.exports = {
   mode: 'development',
@@ -30,6 +30,15 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'src'),
     compress: true,
     historyApiFallback: true,
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      modules: false,
+    },
     port: 3000,
     proxy: {
       [target]: host,
@@ -40,4 +49,4 @@ module.exports = {
       template: __dirname + '/public/index.html',
     }),
   ],
-};
+}
